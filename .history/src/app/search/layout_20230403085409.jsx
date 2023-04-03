3ad6d0f6a1ca513d1602d772666bec52d183f98e
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-04-03 08:55:09 +0100

    created search header component

--- .history/src/app/search/layout_20230403085409.jsx
+++ .history/src/app/search/layout_20230403085409.jsx
@@ -10,7 +10,7 @@ export default function SearchLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <SearchHeader />
+        
         {children}
         {/* footer */}
        
