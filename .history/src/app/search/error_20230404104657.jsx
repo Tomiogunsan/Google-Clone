8bc93af5994360af45d09c59c148dae2c123eab1
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-04-04 10:49:07 +0100

    tested the error component

---
+++ .history/src/app/search/error_20230404104657.jsx
@@ -0,0 +1,14 @@
+'use client'
+import React, { useEffect } from 'react'
+
+export default function Error({error, reset}) {
+    useEffect(() => {
+        console.log('Error: ', error)
+    }, [error])
+  return (
+    <div className='flex flex-col justify-center item'>
+        <h1>Something went wrong</h1>
+        <button onClick={()=> reset()}>Try again</button>
+    </div>
+  )
+}
--- .history/src/app/search/error_20230404104726.jsx
+++ .history/src/app/search/error_20230404104726.jsx
@@ -8,7 +8,7 @@ export default function Error({error, reset}) {
   return (
     <div className='flex flex-col justify-center items-center pt-10'>
         <h1 className='text-3xl mb-4'>Something went wrong</h1>
-        <button className='text-blue-500' onClick={()=> reset()}>Try again</button>
+        <button onClick={()=> reset()}>Try again</button>
     </div>
   )
 }
