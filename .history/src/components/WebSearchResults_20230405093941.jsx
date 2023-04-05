743bab22df3e93775e58e8622f2cd96823565275
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-04-05 09:42:45 +0100

    added styles to the first paragraph

--- .history/src/components/WebSearchResults_20230405093941.jsx
+++ .history/src/components/WebSearchResults_20230405093941.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function WebSearchResults({results}) {
   return (
-    <div className='w-full mx-auto px-3 pb-24 sm:pl-[5%] md:pl-[14%] lg:pl-52'>
+    <div clas>
         <p className='text-gray-600 text-sm mb-5 mt-3'>
             About {results.searchInformation?.formattedTotalResults}
         results ({results.searchInformation?.formattedSearchTime} seconds)
