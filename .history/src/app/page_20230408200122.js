5627ec43cf1eb090c912d8258d9537d21981d627
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-04-08 20:01:44 +0100

    fix

---
+++ .history/src/app/page_20230408200122.js
@@ -0,0 +1,19 @@
+export const dynamic = 'force-dynamic';
+
+import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <>
+    {/* header */}
+    <HomeHeader />
+
+    {/* body */}
+    
+    </div>
+    </>
+  )
+}
---
+++ .history/src/app/page_20230408200125.js
@@ -0,0 +1,26 @@
+export const dynamic = 'force-dynamic';
+
+import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <>
+    {/* header */}
+    <HomeHeader />
+
+    {/* body */}
+    <div className="flex flex-col items-center mt-24">
+      <Image
+      alt='' 
+      width='300'
+      height='100'
+      src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png'
+      />
+      <HomeSearch/>
+    </div>
+    </>
+  )
+}
